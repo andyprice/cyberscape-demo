3a4a1fcd93039dc2c0b07a2e3b8ce12925d9f2d7
Andrew Price <andy@adpx.net>
2022-04-22 18:56:57 +0100

    Add a cache to reduce rng usage
    No need to keep generating randomness, we can just loop over 512 chunks
    once they've been generated.

--- scripts/BlockGenerator.gd
+++ scripts/BlockGenerator.gd
@@ -1,5 +1,9 @@
 extends Node
 
+var cache_size = 512 # Chunks
+var cache = []
+var cache_idx = 0;
+
 var rng = RandomNumberGenerator.new()
 func _ready():
 	rng.randomize()
@@ -9,9 +13,16 @@ func load(_path):
 
 func get_chunk(length):
 	var chunk = []
-	for i in length:
-		var block_type = rng.randi_range(0, 14)
-		chunk.append(block_type)
+	if cache.size() < cache_size:
+		for i in length:
+			var block_type = rng.randi_range(0, 14)
+			chunk.append(block_type)
+		cache.append(chunk)
+	else:
+		chunk = cache[cache_idx]
+		cache_idx += 1
+		if cache_idx >= cache_size:
+			cache_idx = 0
 	return chunk
 
 func prune():
